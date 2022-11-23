ee2fa8a2d1d55dd7761a9d238ffd23e68a7e0c12
mikyrenato <mikyrenato@yahoo.com>
2022-11-23 13:42:58 +0000

    Create database models

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['my4thproject.herokuapp.com', 'lcoalhost']
+ALLOWED_HOSTS = ['my4thproject.herokuapp.com', 'localhost']
 
 
 # Application definition
